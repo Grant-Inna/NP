3fe980e3185555a6ea756f6a9cdbac4e643f8116
Inna Grant <i.grant@ficto.ru>
2023-02-16 20:45:08 +0300

    адаптив главной

--- develop/assets/js/concat/slick.init.js
+++ develop/assets/js/concat/slick.init.js
@@ -39,6 +39,44 @@ $(document).ready(function(){
       });
    }
    
+   const $slider_cell = $('.tile_federal__row');
+   
+   if ($slider_cell.length > 0) {
+      
+      $slider_cell
+      .on("destroy", function(_, slick) {
+         const reinit = debounce(() => {
+            if (slick.activeBreakpoint < window.innerWidth) return;
+            $(this).slick(slick.options);
+            window.removeEventListener("resize", reinit);
+         }, 200);
+         window.addEventListener("resize", reinit);
+      })
+      .slick({
+         slidesToShow: 1,
+         slidesToScroll: 1,
+         speed: 400,
+         arrows: true,
+         mobileFirst: true,
+         infinite: true,
+         autoplay: false,
+         responsive: [
+            {
+               breakpoint: 770,
+               settings: "unslick"
+            },
+            {
+               breakpoint: 580,
+               settings: {
+                  arrows: true,
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+               }
+            }
+         ]
+      });
+   }
+   
    
    
 });
