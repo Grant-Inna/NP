01e42b417041f2901bd42259da8885b24a2ec104
Inna Grant <i.grant@ficto.ru>
2023-03-30 18:04:55 +0300

    изменили название фото

---
+++ build/assets/js/object.js
@@ -0,0 +1,22 @@
+const object = [
+    {
+        id: 0,
+        text: 'Все объекты инфраструктуры'
+    },
+    {
+        id: 1,
+        text: 'bug'
+    },
+    {
+        id: 2,
+        text: 'duplicate'
+    },
+    {
+        id: 3,
+        text: 'invalid'
+    },
+    {
+        id: 4,
+        text: 'wontfix'
+    }
+];
--- develop/assets/js/concat/select.init.js
+++ develop/assets/js/concat/select.init.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
-    $('select').select2();
+  
+    let $region = $('#region'),
+        $object = $('#object');
+    
+    $region.select2({
+        data: region,
+    });
+    $region.on('select2:select', function (e) {
+        let data = e.params.data;
+
+        let id = data.id,
+            text = data.text;
+        console.log(id);
+        console.log(text);
+    });
+    
+    $object.select2({
+        data: object
+    });
+    $object.on('select2:select', function (e) {
+        let data = e.params.data;
+
+        let id = data.id,
+            text = data.text;
+        console.log(id);
+        console.log(text);
+    });
 });
