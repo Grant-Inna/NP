08b2ce5e3f61069c1f5025d8b36c4cca577560c7
Inna Grant <i.grant@ficto.ru>
2023-09-14 16:06:56 +0300

    feat: правки letsee + правки в slick для главной

--- build/assets/js/concat.js
+++ build/assets/js/concat.js
@@ -2039,40 +2039,66 @@ $(document).ready(function () {
    if ($slider_cell.length > 0) {
       if (!$('body').hasClass('letsee-active')) {
       
-      $slider_cell
-      .on("destroy", function (_, slick) {
-         const reinit = debounce(() => {
-            if (slick.activeBreakpoint < window.innerWidth) return;
-            $(this).slick(slick.options);
-            window.removeEventListener("resize", reinit);
-         }, 200);
-         window.addEventListener("resize", reinit);
-      })
-      .slick({
-         slidesToShow: 1,
-         slidesToScroll: 1,
-         speed: 400,
-         arrows: false,
-         dots: true,
-         mobileFirst: true,
-         infinite: true,
-         autoplay: false,
-         responsive: [
-            {
-               breakpoint: 770,
-               settings: "unslick"
-            },
-            {
-               breakpoint: 580,
-               settings: {
-                  arrows: false,
-                  dots: true,
-                  slidesToShow: 2,
-                  slidesToScroll: 2
+         $slider_cell
+         .on("destroy", function (_, slick) {
+            const reinit = debounce(() => {
+               if (slick.activeBreakpoint < window.innerWidth) return;
+               $(this).slick(slick.options);
+               window.removeEventListener("resize", reinit);
+            }, 200);
+            window.addEventListener("resize", reinit);
+         })
+         .slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            speed: 400,
+            arrows: false,
+            dots: true,
+            mobileFirst: true,
+            infinite: true,
+            autoplay: false,
+            responsive: [
+               {
+                  breakpoint: 770,
+                  settings: "unslick"
+               },
+               {
+                  breakpoint: 580,
+                  settings: {
+                     arrows: false,
+                     dots: true,
+                     slidesToShow: 2,
+                     slidesToScroll: 2
+                  }
                }
-            }
-         ]
-      });
+            ]
+         });
+      } else {
+         $slider_cell.slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            speed: 400,
+            arrows: false,
+            dots: true,
+            mobileFirst: true,
+            infinite: true,
+            autoplay: false,
+            responsive: [
+               {
+                  breakpoint: 770,
+                  settings: "unslick"
+               },
+               {
+                  breakpoint: 580,
+                  settings: {
+                     arrows: false,
+                     dots: true,
+                     slidesToShow: 1,
+                     slidesToScroll: 1
+                  }
+               }
+            ]
+         })
       }
    }
 });
